87f392d2c414e211874e73b55375ae3301eefc3e
Asmaa26189 <108579670+Asmaa26189@users.noreply.github.com>
2022-08-23 13:43:09 +0200

    change

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "udagramProject",
+  apiHost: "Udagramproject-env.eba-jh6hgzru.us-east-1.elasticbeanstalk.com",
 };
 
 /*
