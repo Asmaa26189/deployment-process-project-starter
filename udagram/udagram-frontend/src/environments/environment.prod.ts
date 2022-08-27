7af5958fe5d550ae078d6489b147695c11fac626
Asmaa26189 <108579670+Asmaa26189@users.noreply.github.com>
2022-08-27 17:20:51 +0200

    change

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// add enviroment appName from aws ebs Application name
+// add enviroment apiHost from aws ebs host
 export const environment = {
   production: true,
   appName: "udagramProject",
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -2,6 +2,8 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// add enviroment appName from aws ebs Application name
+// add enviroment apiHost from aws ebs host
 export const environment = {
   production: false,
   appName: 'udagramProject',
