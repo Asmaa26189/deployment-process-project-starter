1fd06c2db3509b4b9074ecd4c09e3a456ddcdf7c
Asmaa26189 <108579670+Asmaa26189@users.noreply.github.com>
2022-08-27 16:39:29 +0200

    change

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,18 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID=
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=
+export AWS_DEFAULT_REGION=
+export AWS_SECRET_ACCESS_KEY=
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export POSTGRES_PORT=
+export EB_APP=
+export EB_ENV=
+export JWT_SECRET=
+export PORT=
+export URL=
